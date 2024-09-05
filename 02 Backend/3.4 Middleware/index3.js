02a340fbf2b7a434b77bb26d95353b66464eb8ea
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-05 14:15:38 +0530

    h

--- 02 Backend/3.4 Middleware/index3.js
+++ 02 Backend/3.4 Middleware/index3.js
@@ -3,7 +3,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+function logger(req,res,next){
+  console.log(req.url);
+  console.log(req.method);
+  next();
+}
+
 app.use(logger);
+
+// function logger(req, res, next) {
+//   console.log("Request Method: ", req.method);
+//   console.log("Request URL: ", req.url);
+//   next();
+// }
+
+// app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
