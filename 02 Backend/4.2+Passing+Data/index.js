80c678a276bb75eb488ac0b503a3104615914313
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-05 17:36:00 +0530

    fxc

--- 02 Backend/4.2+Passing+Data/index.js
+++ 02 Backend/4.2+Passing+Data/index.js
@@ -4,11 +4,21 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let startagain = true
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render('index.ejs');
+  
 
-app.post("/submit", (req, res) => {});
+});
+
+app.post("/submit", (req, res) => {
+  const total = req.body["fName"].length + req.body["lName"].length;
+  res.render('index.ejs', { numberOfLetters: total });
+  console.log(req.body)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
