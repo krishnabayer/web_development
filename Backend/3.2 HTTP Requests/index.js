50b95c1c520383379b1849140f70e142b8910b30
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-05 12:35:47 +0530

    bfds

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -1,9 +1,31 @@
-import express from "express";
-const app = express();
+// import express from "express";
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello</h1>");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About Me</h1><p>My name is Angela</p>");
+// });
+
+// app.get("/contact", (req, res) => {
+//   res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
+
+import exppress from 'express';
+
+const app = exppress();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.get("/", (req,res) => {
+  res.send('<h1>hi there</h1>');
+  console.log(req);
 });
 
 app.get("/about", (req, res) => {
