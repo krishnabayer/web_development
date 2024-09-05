65bd5ab65686ab4705019d67683fe976522ee1ff
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-05 14:32:25 +0530

    rsed

--- 02 Backend/3.4 Middleware/index4.js
+++ 02 Backend/3.4 Middleware/index4.js
@@ -1,7 +1,22 @@
 import express from "express";
-
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/public/index.html");
+  console.log(__dirname);
+});
+
+app.post('/submit',(req,res) => {
+  res.send( 'band name : '+ req.body['pet'] +  req.body['street'] )
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
