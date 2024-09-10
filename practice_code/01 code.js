526bba829247a6a911ff5ef01b5503a23e495bc3
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-10 17:30:18 +0530

    sa

---
+++ practice_code/01 code.js
@@ -0,0 +1,18 @@
+// try{
+//     throw ReferenceError()
+// }
+// catch(err) {
+//     console.log(err)
+// }
+
+// throw new Error();
+// console.log("Hello");
+// try {
+    
+// var str = "Hello";
+// str.match("jello");
+try {
+    Number(5).toPrecision(300)
+} catch(e) {
+    console.log("There was an error")
+}
