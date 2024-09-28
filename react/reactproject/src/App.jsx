98956918d14587b4b83d2dbe7df390cd13fa445d
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-09-28 23:24:40 +0530

    cgfhv

--- react/reactproject/src/App.jsx
+++ react/reactproject/src/App.jsx
@@ -1,5 +1,11 @@
+import Hello from "./components/Hello";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div classname="App">
+      <Hello />
+    </div>
+  );
 }
 
 export default App;
