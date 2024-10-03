2ff9e520fbb0e79783162b9d6006aefe1b3aa59d
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-03 20:08:22 +0530

    vfd

--- react/reactproject/src/components/Counter.jsx
+++ react/reactproject/src/components/Counter.jsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setcount] = useState(0);
+  const [increamentby, incrementfunc] = useState(1);
 
   function increase() {
-    setcount(count + 1);
+    let in_ = count + increamentby;
+    setcount(in_);
   }
 
   function decrease() {
-    setcount(count - 1);
+    let de_ = count - increamentby;
+    setcount(de_);
+  }
+
+  function increament() {
+    incrementfunc(increamentby + 1);
+  }
+
+  function decreament() {
+    incrementfunc(increamentby - 1);
   }
 
   return (
@@ -16,6 +27,10 @@ export default function Counter() {
       <h1>the count is {count}</h1>
       <button onClick={increase}>Increament</button>
       <button onClick={decrease}>decreament</button>
+
+      <h1>We are increasing the increament by {increamentby}</h1>
+      <button onClick={increament}>Increament</button>
+      <button onClick={decreament}>decreament</button>
     </div>
   );
 }
