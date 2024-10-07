acb5605b39fe4856b4f184baa501f4cf1afd85d9
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-07 19:38:47 +0530

    vfdcx

--- react/reactproject/src/components/Todoitem.jsx
+++ react/reactproject/src/components/Todoitem.jsx
@@ -5,10 +5,24 @@ export default function Todoitems({ item, todos, settodos }) {
     settodos(todos.filter((todo) => todo !== item));
     // console.log(todos.filter((todo) => todo !== item));
   }
+
+  function handleClick(name) {
+    console.log(name);
+    const newtodos = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    settodos(newtodos);
+    console.log(todos);
+  }
+
+  const complete_style = item.done === true ? styles.completed : "";
+
   return (
     <div className={styles.item}>
       <div className={styles.itemname}>
-        {item.name}
+        <span className={complete_style} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handledelete(item)}
--- react/reactproject/src/components/Todolist.jsx
+++ react/reactproject/src/components/Todolist.jsx
@@ -5,7 +5,12 @@ export default function Todolist({ todos, settodos }) {
   return (
     <div className={styles.list}>
       {todos.map((item, index) => (
-        <Todoitems key={item} item={item} todos={todos} settodos={settodos} />
+        <Todoitems
+          key={item.name}
+          item={item}
+          todos={todos}
+          settodos={settodos}
+        />
       ))}
     </div>
   );
