c7b7e74d819985b1af9a6f75ffb71c28ee72ffb3
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-07 20:18:09 +0530

    asdfghjkl

--- react/reactproject/src/components/Todolist.jsx
+++ react/reactproject/src/components/Todolist.jsx
@@ -2,9 +2,12 @@ import Todoitems from "./Todoitem";
 import styles from "./todolist.module.css";
 
 export default function Todolist({ todos, settodos }) {
+  const sortedtodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item, index) => (
+      {sortedtodos.map((item, index) => (
         <Todoitems
           key={item.name}
           item={item}
