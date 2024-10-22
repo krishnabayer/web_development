a5102484c651868a1a9e4b3396f86cbd355110ef
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-22 11:57:37 +0530

    gfredsa

--- react/reacr-reducer/src/App.jsx
+++ react/reacr-reducer/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useReducer } from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
+  function reducer(state, action) {
+    if (action.type == "increment") {
+      return { ...state, count: state.count + state.incrementBy };
+    }
+    if (action.type == "decrement") {
+      return { ...state, count: state.count - state.incrementBy };
+    }
+    if (action.type == "incrementBy") {
+      return { ...state, incrementBy: action.payload };
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>use reducer {state.count}</h1>
+      <input
+        type="text"
+        value={state.incrementBy}
+        onChange={(e) =>
+          dispatch({ type: "incrementBy", payload: Number(e.target.value) })
+        }
+      />
+      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+        increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+        Decrement
+      </button>
     </div>
-  )
+  );
 }
-
-export default App
