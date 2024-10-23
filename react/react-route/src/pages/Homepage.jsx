65c68d54eb907851201c590b461c657b72d97d7f
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-23 13:05:41 +0530

    das

--- react/react-route/src/pages/Homepage.jsx
+++ react/react-route/src/pages/Homepage.jsx
@@ -1,4 +1,20 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 export default function Homepage() {
-  return <div>Homepage</div>;
+  const [username, setusername] = useState();
+  const navigate = useNavigate();
+  function handleclick() {
+    navigate("/dashboard/profile", { state: { username } });
+  }
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setusername(e.target.value)}
+      />
+      <button onClick={handleclick}>go to profile page</button>
+    </div>
+  );
 }
--- react/react-route/src/pages/Profile.jsx
+++ react/react-route/src/pages/Profile.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export default function profile() {
+  const location = useLocation();
+  const { username } = location.state || {};
   return (
     <div>
       <h1>profile</h1>
+      <h2>my name : {username}</h2>
     </div>
   );
 }
