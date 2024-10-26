2f21098f8fb175e1232322543d5bba5b22d73574
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-10-26 12:32:10 +0530

    fx

---
+++ react/nodejs-crash-course/server.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const port = 8000;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  //   res.write("hello world");
+  res.end("<h1>hello world<h1/>");
+});
+
+server.listen(port, () => {
+  console.log(`server running on port : ${port}`);
+});
