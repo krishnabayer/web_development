75d6a69b3a5d126815576df670a0b10df30aed5a
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-11-12 19:39:57 +0530

    gvfd

---
+++ next_js/routing/app/products/[productsid]/review/[reviewid]/not-found.tsx
@@ -0,0 +1,4 @@
+export default function not_found(){
+    return <h1>review not found</h1>
+    
+}
--- next_js/routing/app/products/[productsid]/review/[reviewid]/page.tsx
+++ next_js/routing/app/products/[productsid]/review/[reviewid]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from "@/node_modules/next/navigation"
 export default function Productsreviewiddetails({params}){
+    if ( parseInt(params.reviewid)>1000){
+         notFound();
+    }
+
     return <>
     <h1>products review {params.reviewid}</h1>
    
