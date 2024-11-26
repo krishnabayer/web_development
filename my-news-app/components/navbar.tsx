a0b97d01db23fee8643694403298af061b938900
krishnabayer <krishna.kshirsagar1@bayer.com>
2024-11-26 18:07:40 +0530

    b vcx

--- my-news-app/components/navbar.tsx
+++ my-news-app/components/navbar.tsx
@@ -1,9 +1,50 @@
 // components/Navbar.js
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 
-export default function Navbar(){
-  return <nav className="fixed">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
-  </nav>
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed w-full h-12 shadow-md">
+      <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
+        <Link href="/">
+          <h1 className="font-bold">SCIPI-AI</h1>
+        </Link>
+        <div className="hidden sm:flex gap-10 ">
+          <Link href="/news">
+            <h1 className="hover:backdrop-blur-lg hover:bg-red/20 rounded">
+              News Overview
+            </h1>
+          </Link>
+          <Link href="/competitor_overview">
+            <h1 className="hover:backdrop-blur-lg hover:bg-red/20 rounded">
+              Competitor Overview
+            </h1>
+          </Link>
+          <Link href="/hubble_qa">
+            <h1 className="hover:backdrop-blur-lg hover:bg-red/20 rounded">
+              HubbleQ&A
+            </h1>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
